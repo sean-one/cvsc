ab35e232a96755e031d5b7ba199063285c023148
SeanOne <basement365@gmail.com>
2020-02-19 18:36:29 -0800

    removed seeds

--- data/seeds/10_eventFilter.js
+++
@@ -1,9 +0,0 @@
-
-exports.seed = function(knex, Promise) {
-  return knex('event_filter').truncate()
-    .then(function () {
-      return knex('event_filter').insert([
-        { user_id: 3, event_id: 1 },
-      ]);
-    });
-};
